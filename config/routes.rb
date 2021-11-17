0d5b681bdda58be2899f4341ba6fb677294117a5
Patrick Ranchez <ranchezpatrick@gmail.com>
2021-11-18 02:53:41 +0800

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :articles
+root 'articles#index'
+  # get '/articles' => 'articles#index'
+  # #get '/articles/:id' => 'articles#show' 
+  
+  # get '/articles/new' => 'articles#new'
+  # post'/articles' => 'articles#create' 
+
+  # get 'articles/:id/edit' => 'articles#edit'
 end
